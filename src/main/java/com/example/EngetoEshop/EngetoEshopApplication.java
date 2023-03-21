cef203fa9790d2044f15d7885fdb605eefd3c054
Worsty <zubalik.k@seznam.cz>
2023-03-21 22:31:02 +0100

    Do main class pridane jmeno na discordu.

--- src/main/java/com/example/EngetoEshop/EngetoEshopApplication.java
+++ src/main/java/com/example/EngetoEshop/EngetoEshopApplication.java
@@ -3,6 +3,7 @@ package com.example.EngetoEshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//jméno na discordu: Karel Zubalík
 @SpringBootApplication
 public class EngetoEshopApplication {
 
